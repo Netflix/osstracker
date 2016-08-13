fe24af5ad45298415dc04c4387d0c91131130cd3
Andrew Spyker <awspyker@gmail.com>
2016-08-12 21:35:24 -0700

    start work on dockerhub builds

--- buildViaTravis.sh
+++ buildViaTravis.sh
@@ -7,6 +7,9 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
   ./gradlew -Prelease.travisci=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" build snapshot 
+  if [ "$TRAVIS_BRANCH" ] == "dockerbuilds" ]; then
+    echo -e "should do docker build and push"
+  fi
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   case "$TRAVIS_TAG" in
