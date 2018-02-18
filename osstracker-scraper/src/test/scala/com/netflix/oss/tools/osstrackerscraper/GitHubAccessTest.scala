56e06030cf21990a435f359817e37b13e3f8ccf5
Andrew Spyker <awspyker@gmail.com>
2018-02-18 13:09:58 -0500

    add filter by standard github tags, upgrade scala and scala libraries

--- osstracker-scraper/src/test/scala/com/netflix/oss/tools/osstrackerscraper/GitHubAccessTest.scala
+++ osstracker-scraper/src/test/scala/com/netflix/oss/tools/osstrackerscraper/GitHubAccessTest.scala
@@ -13,11 +13,19 @@ class GitHubAccessTest extends FunSuite {
     val mapper = new ObjectMapper
     mapper.setVisibilityChecker(new Std(NONE, NONE, NONE, NONE, ANY));
     mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
     val issuesJSON = scala.io.Source.fromResource("security_monkey-issues.json").mkString
     val issues = mapper.readValue(issuesJSON, classOf[Array[GHIssue]])
 
     val access = new GithubAccess("a", "a", false)
+
     val stats = access.getIssuesStats(new Array[GHIssue](0), issues)
     assert(stats.openCountReallyOpen == 23)
+
+    val issuesJSON2 = scala.io.Source.fromResource("hollow-issues.json").mkString
+    val issues2 = mapper.readValue(issuesJSON2, classOf[Array[GHIssue]])
+
+    val stats2 = access.getIssuesStats(new Array[GHIssue](0), issues2)
+    assert(stats2.openCountReallyOpen == 13)
   }
 }
